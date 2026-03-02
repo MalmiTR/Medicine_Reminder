fc9772dcdafd89e181fe2c472bd17a15abed1191
dinviiii <dinushabesingha114@gmail.com>
2026-03-02 23:40:13 +0530

    Added Dashboard and View/Edit/Delete Medicine screen

--- app/src/main/java/com/example/medicine_reminder/LoginActivity.java
+++ app/src/main/java/com/example/medicine_reminder/LoginActivity.java
@@ -42,7 +42,16 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Invalid login", Toast.LENGTH_SHORT).show();
         }
+        if (c.moveToFirst()) {
+            int userId = c.getInt(0);
+
+            Intent i = new Intent(this, DashboardActivity.class);
+            i.putExtra("USER_ID", userId);
+            startActivity(i);
+            finish();
+        }
     }
+
 
     public void goRegister(View v) {
         // Temporarily comment out until RegisterActivity is created
